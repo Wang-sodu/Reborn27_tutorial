e552072f05ec653a4188924bc2b1c825960e0dfb
Wang-sodu <3901495272@qq.com>
2026-07-08 22:04:31 +0800

    imshow

--- src/publisher.cpp
+++ src/publisher.cpp
@@ -1,6 +1,12 @@
 #include <chrono>
 #include <string>
 #include <thread>
+#include <vector>
+
+
+#include <opencv2/highgui.hpp>
+
+#include "camera/camera.hpp"
 
 #include "zenoh.hxx"
 
@@ -10,14 +16,33 @@ int main() {
 
     // 2. 声明发布者 (指定键)
     auto publisher = session.declare_publisher("demo/topic");
+    
+     io::Camera camera(io::HikRobotParams{
+    .exposure_ms = 5.0,
+    .gain = 16.0,
+    .vid_pid = "2bdf:0001",
+    .adcbitdepth = 8,
+    .framerate = 265.0,
+    .trigger_mode = false,
+  });
 
     // 3. 发送数据
     while (true) {
         auto now = std::chrono::system_clock::now();
         auto ms = std::chrono::duration_cast<std::chrono::milliseconds>(now.time_since_epoch()).count();
 
-        std::string message = "hello from " + std::to_string(ms);
-        publisher.put(message);
+        cv::Mat img;
+        std::chrono::steady_clock::time_point timestamp;
+        camera.read(img, timestamp);
+       std::vector<uchar> buffer;
+        std::vector<int> compression_params;
+        compression_params.push_back(cv::IMWRITE_JPEG_QUALITY);
+        compression_params.push_back(80);  // 质量80%
+        cv::imencode(".jpg", img, buffer, compression_params);
+
+        // 修改：发送图像数据而不是字符串
+        std::string image_data(buffer.begin(), buffer.end());
+            publisher.put(image_data);
         std::this_thread::sleep_for(std::chrono::seconds(1));
     }
     return 0;
--- src/subscriber.cpp
+++ src/subscriber.cpp
@@ -1,11 +1,32 @@
 #include <cstdio>
 #include <iostream>
+#include <vector>
+
+
+#include <opencv2/highgui.hpp>
+
+
 
 #include "zenoh.hxx"
 
 // 1. 定义一个回调函数，参数必须是 zenoh::Sample&
 void on_data_received(zenoh::Sample& sample) {
-    std::cout << sample.get_payload().as_string() << std::endl;
+    // 获取payload数据
+    const auto& payload = sample.get_payload();
+    std::string str_data = payload.as_string();
+        std::vector<uchar> data(str_data.begin(), str_data.end());
+    
+    // 解码JPEG为图像
+    cv::Mat img = cv::imdecode(data, cv::IMREAD_COLOR);
+    
+    if (!img.empty()) {
+        // 显示图像
+        cv::imshow("Camera Stream", img);
+        cv::waitKey(1);
+        std::cout << "收到图像，大小: " << data.size() << " bytes" << std::endl;
+    } else {
+        std::cerr << "解码图像失败" << std::endl;
+    }
 }
 
 int main() {
